01902dc4b99fbcc3970efee5352c3d44d6b1169c
prahlad122 <prahladji1592003@gmail.com>
2025-07-18 22:42:24 +0530

    commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "163ac009d76df05b3245425a8d7e46b4",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
